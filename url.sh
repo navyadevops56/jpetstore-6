2dbc753dcd8d9b53ac6b96bd419b71a8fd9e6a52
navyadevops56 <90696748+navyadevops56@users.noreply.github.com>
2021-10-07 17:28:51 +0530

    Update url.sh

--- url.sh
+++ url.sh
@@ -1,1 +1,1 @@
-echo http://$(kubectl get nodes -o wide | awk 'NR==2{print $7}' ):$(kubectl get svc |awk 'NR==2{print $5}' | cut -c4-8)
+echo http://$(kubectl get nodes -o wide | awk 'NR==2{print $7}' ):$(kubectl get svc |awk 'NR==2{print $5}' | cut -c4-8)/jpetstore
